b6fbdb010633bc79da7f057ec2fb6ed72cd90162
amusso01 <eandram@hotmail.it>
2022-06-23 17:49:01 +0200

    mobile contact, services

--- partials/page/services.php
+++ partials/page/services.php
@@ -10,6 +10,36 @@
         <i><?php get_template_part( 'svg-template/svg', 'icon-arrow-orange' ) ?></i>
         <a class="cat-link link-line-hidden" href="#">SEE CASE STUDIES</a>
       </div>
+
+
+      <div class="o-service-tab " data-tab="advertising" >
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-advertising-audience' ) ?></i>
+          <p>AUDIENCE</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-advertising-video-ads' ) ?></i>
+          <p>VIDEO ADS</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-advertising-advertising-strategy' ) ?></i>
+          <p>ADVERTISING STRATEGY</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-advertising-digi-ads' ) ?></i>
+          <p>DIGITAL ADS</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-advertising-media-planning' ) ?></i>
+          <p>MEDIA PLANNING</p>
+        </div>
+          <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-advertising-pr-socialmedia' ) ?></i>
+          <p>PR & SOCIAL MEDIA</p>
+        </div>
+      </div>
+
+
     </div>
 
     <div class="o-cat-item js-cat-item" data-item="service-cat" data-cat="growth">
@@ -21,6 +51,34 @@
         <i><?php get_template_part( 'svg-template/svg', 'icon-arrow-green' ) ?></i>
         <a class="cat-link link-line-hidden" href="#">SEE CASE STUDIES</a>
       </div>
+
+
+      <div class="o-service-tab" data-tab="growth" >
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-growth-branding' ) ?></i>
+          <p>BRANDING</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-growth-sustainable-revenue' ) ?></i>
+          <p>SUSTAINABLE REVENUE</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-growth-goals-priorities' ) ?></i>
+          <p>GOALS & PRIORITIES</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-growth-content' ) ?></i>
+          <p>CONTENT</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-growth-custom-ads' ) ?></i>
+          <p>CUSTOM ADS</p>
+        </div>
+          <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-growth-market-customer' ) ?></i>
+          <p>MARKET & CUSTOMER</p>
+        </div>
+      </div>
     </div>
 
     <div class="o-cat-item js-cat-item" data-item="service-cat" data-cat="creative">
@@ -31,6 +89,33 @@
       <div class="cat-arrow">
         <i><?php get_template_part( 'svg-template/svg', 'icon-arrow-purple' ) ?></i>
         <a class="cat-link link-line-hidden" href="#">SEE CASE STUDIES</a>
+      </div>
+
+      <div class="o-service-tab" data-tab="creative" >
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-creative-branding' ) ?></i>
+          <p>BRANDING</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-creative-creative' ) ?></i>
+          <p>CREATIVE</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-creative-public-ads' ) ?></i>
+          <p>PUBLIC ADS</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-creative-digital' ) ?></i>
+          <p>DIGITAL</p>
+        </div>
+        <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-creative-social-media' ) ?></i>
+          <p>SOCIAL MEDIA</p>
+        </div>
+          <div class="o-service">
+          <i><?php get_template_part( 'svg-template/svg', 'rebel-lion-creative-editorial' ) ?></i>
+          <p>EDITORIAL</p>
+        </div>
       </div>
     </div>
   </div>
