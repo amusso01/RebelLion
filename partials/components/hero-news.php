036253303fd49d853bccd98b2b252738f184f9d5
amusso01 <eandram@hotmail.it>
2022-10-04 16:20:05 +0200

    hero news - added ACF for title

--- partials/components/hero-news.php
+++ partials/components/hero-news.php
@@ -1,6 +1,7 @@
 <?php 
 $postPageId = get_queried_object(  )->ID; 
 $heroText = get_field('hero_text', 'options');
+$heroTitle= get_field('hero_title', 'options');
 ?>
 
 <section class="p-page-hero" style="background-color:#000000;" data-scroll-section>
@@ -8,7 +9,7 @@ $heroText = get_field('hero_text', 'options');
 
     <div class="l-container--small">
       <div class="text-container">
-        <h1 class="stroke-text" >AGENCY INSIGHTS</h1>
+        <h1 class="stroke-text" >       <?php echo $heroTitle ?></h1>
 
 
         <div data-scroll data-scroll-speed=1 class="hero-text">
--- single.php
+++ single.php
@@ -18,6 +18,10 @@ get_header();
 			<!-- PAGE HERO -->
 			<?php get_template_part( 'partials/projects/single-hero' ); ?>
 
+			<section class="editor l-container--small" style="padding-top:90px">
+				<?php the_content() ?>
+			</section>
+
 			<!-- PAGE CONTENT -->
 			<?php get_template_part( 'partials/projects/preview-single' ); ?>
 							
